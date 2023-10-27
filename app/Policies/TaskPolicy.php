9582be1efe88fc3e77ec3a164088e3aaf049e515
Mina Nabil <mina9492@hotmail.com>
2023-10-27 12:27:40 +0200

    merged

--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -65,7 +65,7 @@ class TaskPolicy
      */
     public function delete(User $user, Task $task)
     {
-        return $user->id == 1 || $user->id == 10 || $user->id == 11;
+        return $user->id == 1 || $user->id == 11;
     }
 
 }
