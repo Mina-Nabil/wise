8c339ca961f981b95850b9a08fe1c1d5e07bbb4b
Mina Nabil <mina9492@hotmail.com>
2023-11-09 09:00:57 +0200

    removing last commit

--- database/migrations/2023_09_24_095819_create_notifications_table.php
+++ database/migrations/2023_09_24_095819_create_notifications_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('route');
             $table->text('message')->nullable();
+            $table->dateTime('seen_at')->nullable();
             $table->timestamps();
         });
     }
