90d4f5dee1584a8a8e3efc6d3d09e206a04744ef
Michael Rafaillo <michael.rafaillo@gmail.com>
2024-01-15 22:34:46 +0200

    adding change customer status

--- app/Http/Livewire/CustomerIndex.php
+++ app/Http/Livewire/CustomerIndex.php
@@ -4,12 +4,12 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Customers\Customer;
+use App\Models\Customers\Status;
 use App\Models\Customers\Profession;
 use App\Models\Base\Country;
 use App\Traits\AlertFrontEnd;
 use Livewire\WithPagination;
 use App\Traits\ToggleSectionLivewire;
-
 
 class CustomerIndex extends Component
 {
@@ -36,92 +36,106 @@ class CustomerIndex extends Component
     public $LeadName;
     public $LeadPhone;
 
-    public function addLead(){
+    public $changeCustStatusId;
+    public $changeCustStatusStatus;
+    public $statusReason;
+    public $statusNote;
+
+    public function changeThisStatus($id, $status)
+    {
+        $this->changeCustStatusId = $id;
+        $this->changeCustStatusStatus = $status;
+    }
+
+    public function closeChangeStatus()
+    {
+        $this->changeCustStatusId = null;
+        $this->changeCustStatusStatus = null;
+    }
+
+    public function changeStatus()
+    {
+        $res = Customer::find($this->changeCustStatusId)->setStatus($this->changeCustStatusStatus,$this->statusReason,$this->statusNote);
+        if ($res) {
+            $this->alert('success', 'Status updated!');
+            $this->changeCustStatusId = null;
+            $this->changeCustStatusStatus = null;
+        } else {
+            $this->alert('failed', 'server error');
+        }
+    }
+
+    public function addLead()
+    {
         $this->validate([
             'LeadName' => 'required|string|max:255',
             'LeadPhone' => 'required|string|max:255',
         ]);
 
         $customer = new Customer();
-        $res = $customer->newLead(
-            $this->LeadName,
-            $this->LeadPhone,
-            null, null, null, null, null,null, null, null, null, null, null,
-            auth()->id()
-        );
-        if($res){
+        $res = $customer->newLead($this->LeadName, $this->LeadPhone, null, null, null, null, null, null, null, null, null, null, null, auth()->id());
+        if ($res) {
             $this->alert('success', 'Lead Added!');
             $this->toggleAddLead();
-        }else{
+        } else {
             $this->alert('failed', 'server error');
         }
     }
 
-    public function addCustomer(){
+    public function addCustomer()
+    {
         $this->validate([
             'name' => 'required|string|max:255',
             'arabic_name' => 'nullable|string|max:255',
             'bdate' => 'nullable|date',
-            'email' =>  'required|email',
-            'gender' =>  'required|in:' . implode(',', Customer::GENDERS),
-            'maritalStatus' =>  'nullable|in:' . implode(',', Customer::MARITALSTATUSES),
-            'idType' =>  'nullable|in:' . implode(',', Customer::IDTYPES),
+            'email' => 'required|email',
+            'gender' => 'required|in:' . implode(',', Customer::GENDERS),
+            'maritalStatus' => 'nullable|in:' . implode(',', Customer::MARITALSTATUSES),
+            'idType' => 'nullable|in:' . implode(',', Customer::IDTYPES),
             'idNumber' => 'nullable|string|max:255',
             'nationalId' => 'nullable|integer|exists:countries,id',
             'profession_id' => 'nullable|exists:professions,id',
             'salaryRange' => 'nullable|in:' . implode(',', Customer::SALARY_RANGES),
-            'incomeSource' =>  'nullable|in:' . implode(',', Customer::INCOME_SOURCES),
+            'incomeSource' => 'nullable|in:' . implode(',', Customer::INCOME_SOURCES),
         ]);
 
         $customer = new Customer();
-        $res = $customer->newCustomer(
-            auth()->id(),
-            $this->name,
-            $this->gender,
-            $this->email,
-            $this->arabic_name,
-            $this->bdate,
-            $this->maritalStatus,
-            $this->idType,
-            $this->idNumber,
-            $this->nationalId,
-            $this->profession_id,
-            $this->salaryRange,
-            $this->incomeSource
-        );
-        if($res){
-            redirect()->route('customers.show',$res->id);
-        }else{
+        $res = $customer->newCustomer(auth()->id(), $this->name, $this->gender, $this->email, $this->arabic_name, $this->bdate, $this->maritalStatus, $this->idType, $this->idNumber, $this->nationalId, $this->profession_id, $this->salaryRange, $this->incomeSource);
+        if ($res) {
+            redirect()->route('customers.show', $res->id);
+        } else {
             $this->alert('failed', 'server error');
         }
     }
 
-    public function toggleAddLead(){
+    public function toggleAddLead()
+    {
         $this->toggle($this->addLeadSection);
     }
 
-    public function toggleAddCustomer(){
+    public function toggleAddCustomer()
+    {
         $this->toggle($this->addCustomerSection);
     }
 
     public function redirectToShowPage($id)
     {
-        return redirect(route('customers.show',  $id));
+        return redirect(route('customers.show', $id));
     }
 
     public function render()
     {
-        
         $GENDERS = Customer::GENDERS;
         $MARITALSTATUSES = Customer::MARITALSTATUSES;
         $IDTYPES = Customer::IDTYPES;
         $SALARY_RANGES = Customer::SALARY_RANGES;
         $INCOME_SOURCES = Customer::INCOME_SOURCES;
         $professions = Profession::all();
+        $customerStatus = Status::STATUSES;
         $countries = Country::all();
         $customers = Customer::userData($this->search)->paginate(10);
 
-        return view('livewire.customer-index',[
+        return view('livewire.customer-index', [
             'customers' => $customers,
             'GENDERS' => $GENDERS,
             'MARITALSTATUSES' => $MARITALSTATUSES,
@@ -130,6 +144,7 @@ class CustomerIndex extends Component
             'SALARY_RANGES' => $SALARY_RANGES,
             'INCOME_SOURCES' => $INCOME_SOURCES,
             'countries' => $countries,
+            'customerStatus' => $customerStatus,
         ]);
     }
 }
--- resources/views/livewire/customer-index.blade.php
+++ resources/views/livewire/customer-index.blade.php
@@ -27,7 +27,8 @@
         <div class="card-body px-6 pb-6">
             <div class=" -mx-6">
                 <div class="inline-block min-w-full align-middle">
-                    <div class="overflow-hidden ">
+                    <div class=" ">
+                        {{-- overflow-hidden --}}
                         <table class="min-w-full divide-y divide-slate-100 table-fixed dark:divide-slate-700">
                             <thead class=" border-t border-slate-100 dark:border-slate-800 bg-slate-200 dark:bg-slate-700">
                                 <tr>
@@ -56,14 +57,18 @@
                                         Arabic Name
                                     </th>
 
+                                    <th scope="col" class=" table-th ">
+                                        Action
+                                    </th>
+
                                 </tr>
                             </thead>
                             <tbody class="bg-white divide-y divide-slate-100 dark:bg-slate-800 dark:divide-slate-700">
 
                                 @foreach ($customers as $customer)
-                                    <tr wire:click="redirectToShowPage({{ $customer }})" class="hover:bg-slate-200 dark:hover:bg-slate-700 cursor-pointer">
+                                    <tr>
 
-                                        <td class="table-td ">
+                                        <td wire:click="redirectToShowPage({{ $customer }})" class="table-td hover:bg-slate-200 dark:hover:bg-slate-700 cursor-pointer">
                                             <b>{{ $customer->name }}</b>
                                         </td>
 
@@ -90,6 +95,24 @@
                                             <b>{{ $customer->arabic_name }}</b>
                                         </td>
 
+                                        <td class="table-td ">
+                                            <div class="relative">
+                                                <div class="dropdown relative">
+                                                    <button class="text-xl text-center block w-full " type="button" id="transactionDropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                                                        <iconify-icon icon="heroicons-outline:dots-vertical"></iconify-icon>
+                                                    </button>
+                                                    <ul class="dropdown-menu min-w-[120px] absolute text-sm text-slate-700 dark:text-white hidden bg-white dark:bg-slate-700 shadow z-[2] float-left overflow-hidden list-none text-left rounded-lg mt-1 m-0 bg-clip-padding border-none">
+                                                        @foreach ($customerStatus as $status)
+                                                        <li class="cursor-pointer" wire:click="changeThisStatus('{{ $customer->id }}','{{ $status }}')">
+                                                                <a  class="text-slate-600 dark:text-white block font-Inter font-normal px-4 py-2 hover:bg-slate-100 dark:hover:bg-slate-600 dark:hover:text-white">
+                                                                    {{ $status }}
+                                                                </a>
+                                                            </li>
+                                                        @endforeach
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </td>
 
                                     </tr>
                                 @endforeach
@@ -350,6 +373,51 @@
         </div>
     @endif
 
+    @if ($changeCustStatusId)
+        <div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto show" tabindex="-1" aria-labelledby="vertically_center" aria-modal="true" role="dialog" style="display: block;">
+            <div class="modal-dialog top-1/2 !-translate-y-1/2 relative w-auto pointer-events-none">
+                <div class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
+                    <div class="relative bg-white rounded-lg shadow dark:bg-slate-700">
+                        <!-- Modal header -->
+                        <div class="flex items-center justify-between p-5 border-b rounded-t dark:border-slate-600 bg-black-500">
+                            <h3 class="text-xl font-medium text-white dark:text-white capitalize">
+                                Update Status
+                            </h3>
+                            <button wire:click="closeChangeStatus" type="button" class="text-slate-400 bg-transparent hover:text-slate-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-slate-600 dark:hover:text-white" data-bs-dismiss="modal">
+                                <svg aria-hidden="true" class="w-5 h-5" fill="#ffffff" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                                </svg>
+                                <span class="sr-only">Close modal</span>
+                            </button>
+                        </div>
+                        <!-- Modal body -->
+                        <div class="p-6 space-y-4">
 
+                            <div class="input-area mb-3">
+                                <label class="form-label">Reason</label>
+                                <input class="form-control py-2 @error('statusReason') !border-danger-500 @enderror" id="default-picker" type="text" wire:model.defer="statusReason" autocomplete="off">
+                                @error('statusReason')
+                                    <span class="font-Inter text-sm text-danger-500 pt-2 inline-block">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div class="input-area mb-3">
+                                <label class="form-label">Note</label>
+                                <input class="form-control py-2 @error('statusNote') !border-danger-500 @enderror" id="default-picker" type="text" wire:model.defer="statusNote" autocomplete="off">
+                                @error('statusNote')
+                                    <span class="font-Inter text-sm text-danger-500 pt-2 inline-block">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+                        <!-- Modal footer -->
+                        <div class="flex items-center justify-end p-6 space-x-2 border-t border-slate-200 rounded-b dark:border-slate-600">
+                            <button wire:click="changeStatus" data-bs-dismiss="modal" class="btn inline-flex justify-center text-white bg-black-500">
+                                Submit
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
 
 </div>
