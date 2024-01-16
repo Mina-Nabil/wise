84588b73b58253d47ae53ffbb413cba34548c8c0
Mina Nabil <mina9492@hotmail.com>
2024-01-16 10:32:49 +0200

    fixing status

--- resources/views/livewire/customer-index.blade.php
+++ resources/views/livewire/customer-index.blade.php
@@ -105,7 +105,7 @@
                                                         @foreach ($customerStatus as $status)
                                                         <li class="cursor-pointer" wire:click="changeThisStatus('{{ $customer->id }}','{{ $status }}')">
                                                                 <a  class="text-slate-600 dark:text-white block font-Inter font-normal px-4 py-2 hover:bg-slate-100 dark:hover:bg-slate-600 dark:hover:text-white">
-                                                                    {{ $status }}
+                                                                    Set as {{ $status }}
                                                                 </a>
                                                             </li>
                                                         @endforeach
