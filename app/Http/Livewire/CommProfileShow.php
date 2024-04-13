aa95950e5561b523dfdd0949eae55f3330dc1a70
Michael Rafaillo <michael.rafaillo@gmail.com>
2024-04-14 01:30:25 +0200

    Commission Profile Show component updates

--- app/Http/Livewire/CommProfileShow.php
+++ app/Http/Livewire/CommProfileShow.php
@@ -5,6 +5,9 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 use App\Models\Payments\CommProfile;
 use App\Models\Users\User;
+use App\Models\Payments\CommProfileConf;
+use App\Models\Insurance\Policy;
+use App\Models\Insurance\Company;
 use Livewire\WithPagination;
 use App\Traits\AlertFrontEnd;
 use App\Traits\ToggleSectionLivewire;
@@ -20,6 +23,60 @@ class CommProfileShow extends Component
     public $updatedUserId;
     public $updatedTitle;
     public $updatedDesc;
+
+    public $editedRowId;
+
+    public $newConfSec;
+    public $percentage;
+    public $from;
+    public $condition;
+    public $line_of_business;
+    public $conditionType = 'company';
+
+    public $searchCon;
+    public $searchlist;
+
+    public function updatedSearchCon(){
+        if($this->conditionType == 'company'){
+            $this->searchlist = Company::searchBy($this->searchCon)->get()->take(5);
+        }elseif($this->conditionType == 'policy'){
+            $this->searchlist = Policy::searchBy($this->searchCon)->get()->take(5);
+        }
+    }
+
+    public function selectResult($id){
+        if($this->conditionType == 'company'){
+            $this->condition = Company::find($id);
+        }elseif($this->conditionType == 'policy'){
+            $this->condition = Policy::find($id);
+        }
+    }
+
+    public function addConf() {
+        if($this->line_of_business){
+
+        }else{
+            if($this->conditionType == 'company'){
+                //
+            }elseif($this->conditionType == 'policy'){
+                //
+            }
+        }
+        
+    }
+
+    public function updatedConditionType(){
+        $this->searchCon = null;
+        $this->searchlist = null;
+    }
+
+    public function openNewConfSection(){
+        $this->newConfSec = true;
+    }
+
+    public function closeNewConfSection(){
+        $this->newConfSec = false;
+    }
 
     public function openUpdateSec()
     {
@@ -71,10 +128,14 @@ class CommProfileShow extends Component
     public function render()
     {
         $profileTypes = CommProfile::TYPES;
+        $FROMS = CommProfileConf::FROMS;
         $users = User::all();
+        $LOBs = Policy::LINES_OF_BUSINESS;
         return view('livewire.comm-profile-show', [
             'profileTypes' => $profileTypes,
             'users' => $users,
+            'FROMS' => $FROMS,
+            'LOBs' => $LOBs
         ]);
     }
 }
