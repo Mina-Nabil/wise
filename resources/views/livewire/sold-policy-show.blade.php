c0fc442d9cb010025881a1abce349f634b416e79
Mina Nabil <mina9492@hotmail.com>
2024-06-25 13:51:11 +0300

    adding offer files to sold policy

--- resources/views/livewire/sold-policy-show.blade.php
+++ resources/views/livewire/sold-policy-show.blade.php
@@ -572,18 +572,6 @@
                                         </li>
                                     @endforeach
 
-
-                                    <div class="border-dashed border dropzone-container cursor-pointer"
-                                        style="border-color: #aeaeae">
-                                        <p class="dropzone-para" wire:loading wire:target="uploadedFile"
-                                            style="font-size:20px"><iconify-icon
-                                                icon="svg-spinners:tadpole"></iconify-icon></p>
-                                        <p class="dropzone-para" wire:loading.remove wire:target="uploadedFile">Choose
-                                            a
-                                            file or drop it here...</p>
-                                        <input name="file" id="fileInput" type="file"
-                                            class="dropzone dropzone-input" multiple wire:model="uploadedFile" />
-                                    </div>
                                 </ul>
 
 
