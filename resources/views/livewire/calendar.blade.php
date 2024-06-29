7ff9f793f83ea18edbae28ffb4af1b06deccfb6b
Mina Nabil <mina9492@hotmail.com>
2024-06-29 21:51:09 +0300

    adding calendar

--- resources/views/livewire/calendar.blade.php
+++ resources/views/livewire/calendar.blade.php
@@ -21,8 +21,7 @@
                 headerToolbar: {
                     left: 'prev,next',
                     center: 'title',
-                    right: 'dayGridMonth',
-                    'timeGridWeek'
+                    right: 'dayGridMonth,timeGridWeek'
                 },
                 // timeZone: 'UTC',
                 selectable: true,
