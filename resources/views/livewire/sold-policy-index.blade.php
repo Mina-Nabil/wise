a07bc7cc4c2294d1381e8b9a7a6404aef47c8c02
Mina Nabil <mina9492@hotmail.com>
2024-07-15 22:53:11 +0300

    raglasha

--- resources/views/livewire/sold-policy-index.blade.php
+++ resources/views/livewire/sold-policy-index.blade.php
@@ -20,11 +20,12 @@
             <div class=" -mx-6">
                 <div class="inline-block min-w-full align-middle">
                     <div class="overflow-hidden ">
-
+                        <input class="form-control py-2 flatpickr flatpickr-input active w-auto ml-5 mb-5" style="width:300px"
+                        id="range-picker" data-mode="range" value="" type="text" readonly="readonly"
+                        wire:model="dateRange">
+                        
                         <div class="flex items-center space-x-7 flex-wrap h-[30px]">
-                            <input class="form-control py-2 flatpickr flatpickr-input active w-auto ml-5 mb-5" style="width:300px"
-                            id="range-picker" data-mode="range" value="" type="text" readonly="readonly"
-                            wire:model="dateRange">
+                       
 
                             <div class="secondary-radio">
                                 <label class="flex items-center cursor-pointer">
