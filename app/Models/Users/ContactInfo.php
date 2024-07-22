fcb81963037de6adf1f5463883484c4ef892c93d
Mina Nabil <mina9492@hotmail.com>
2024-07-22 14:17:20 +0300

    fixing qr code

--- app/Models/Users/ContactInfo.php
+++ app/Models/Users/ContactInfo.php
@@ -49,7 +49,7 @@ class ContactInfo extends Model
             $vcard->addPhoneNumber($this->home_number1, 'HOME');
 
         if ($this->home_number2)
-            $vcard->addPhoneNumber("Hotline:  $this->home_number2");
+            $vcard->addLabel($this->home_number2, "Hotline");
 
         if ($this->work_number1)
             $vcard->addPhoneNumber($this->work_number1, 'WORK');
