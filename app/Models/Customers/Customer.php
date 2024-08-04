914b78d11d4de8a5cea72c48af47e0f7367f23ae
Mina Nabil <mina9492@hotmail.com>
2024-08-04 12:02:45 +0300

    adding leads export/import

--- app/Models/Customers/Customer.php
+++ app/Models/Customers/Customer.php
@@ -692,7 +692,7 @@ class Customer extends Model
 
         $writer = new Xlsx($newFile);
         $file_path = "exports/leads_export.xlsx";
-        $public_file_path = storage_path($file_path);
+        $public_file_path = public_path($file_path);
         $writer->save($public_file_path);
 
         return response()->download($public_file_path)->deleteFileAfterSend(true);
@@ -758,7 +758,7 @@ class Customer extends Model
 
         $writer = new Xlsx($newFile);
         $file_path = "exports/leads_export.xlsx";
-        $public_file_path = storage_path($file_path);
+        $public_file_path = public_path($file_path);
         $writer->save($public_file_path);
 
         return response()->download($public_file_path)->deleteFileAfterSend(true);
