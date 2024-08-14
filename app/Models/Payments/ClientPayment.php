dc6a9cd4763730b66d82ddde938ba933cb029203
Mina Nabil <mina9492@hotmail.com>
2024-08-14 16:35:31 +0300

    fixing due

--- app/Models/Payments/ClientPayment.php
+++ app/Models/Payments/ClientPayment.php
@@ -361,8 +361,8 @@ class ClientPayment extends Model
             ( DATE_ADD( IF( sold_policies.created_at > sold_policies.start, sold_policies.created_at , sold_policies.start), INTERVAL policy_comm_conf.due_penalty DAY) < NOW() 
             AND 
             DATEDIFF( 
-                NOW() ,
-                DATE_ADD( IF( sold_policies.created_at > sold_policies.start, sold_policies.created_at , sold_policies.start), INTERVAL policy_comm_conf.due_penalty DAY)   
+              DATE_ADD( IF( sold_policies.created_at > sold_policies.start, sold_policies.created_at , sold_policies.start), INTERVAL policy_comm_conf.due_penalty DAY) ,  
+              NOW()     
             ) <= $days ) ");
     }
 
