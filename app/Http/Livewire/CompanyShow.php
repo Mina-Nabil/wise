b123c1880e81d60b4df7780b0f68ee5274c26fe5
Mina Nabil <mina9492@hotmail.com>
2024-08-23 17:00:39 +0300

    working on phase 2

--- app/Http/Livewire/CompanyShow.php
+++ app/Http/Livewire/CompanyShow.php
@@ -271,7 +271,7 @@ class CompanyShow extends Component
     public function render()
     {
         $companyEmails = Company::find($this->company->id)->emails()->paginate(20);
-        $soldPolicies = SoldPolicy::userData(searchText: $this->seachAllSoldPolicies)->ByCompany(company_id: $this->company->id)->paginate(25);
+        $soldPolicies = SoldPolicy::userData(searchText: $this->seachAllSoldPolicies)->ByCompany(company_id: $this->company->id)->paginate(10);
         $this->available_policies = SoldPolicy::userData()->ByCompany(company_id: $this->company->id)->paginate(5);
         return view('livewire.company-show', [
             'soldPolicies' => $soldPolicies,
