6b115b734945587f2df6230d6f4fd841bcc09925
Michael Rafaillo <michael.rafaillo@gmail.com>
2024-09-27 01:53:28 +0300

    fixing account show

--- resources/views/layouts/accounting.blade.php
+++ resources/views/layouts/accounting.blade.php
@@ -637,6 +637,45 @@
     <script src="{{ asset('assets/js/rt-plugins.js') }}"></script>
     <script src="{{ asset('assets/js/app.js') }}"></script>
 
+
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <script>
+        $(function() {
+            var start = moment().subtract(4, 'months');
+            var end = moment();
+
+            function cb(start, end) {
+                $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+                // console.log('Start date: ' + start.format('YYYY-MM-DD'));
+                // console.log('End date: ' + end.format('YYYY-MM-DD'));
+
+                Livewire.emit('dateRangeSelected', start.format('YYYY-MM-DD'), end.format('YYYY-MM-DD'));
+            }
+
+            $('#reportrange').daterangepicker({
+                startDate: start,
+                endDate: end,
+                "alwaysShowCalendars": true,
+                ranges: {
+                    'Today': [moment(), moment()],
+                    'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                    'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                    'This Month': [moment().startOf('month'), moment().endOf('month')],
+                    // 'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1,
+                    //     'month').endOf('month')],
+                    'Last 3 Months': [moment().subtract(3, 'months'), moment()],
+                }
+            }, cb);
+
+            cb(start, end);
+
+
+        });
+    </script>
+
     @yield('body')
 
     <script>
--- resources/views/livewire/Accounting/account-show.blade.php
+++ resources/views/livewire/Accounting/account-show.blade.php
@@ -1,12 +1,14 @@
 <div>
     <div class="card">
-        <input type="text" class="form-control w-auto d-inline-block cursor-pointer" style="width:auto" name="datetimes" id="reportrange" />
+        
         <header class=" card-header noborder">
             <div>
                 <p class="text-xs text-slate-500 dark:text-slate-400 mt-1">Start Balance</p>
-                <h4 class="card-title">{{ number_format('2500', 2) }}
+                <h4 class="card-title">{{ $entries->first() ? number_format($entries->first()->account_balance, 2) : number_format(0, 2) }}
                 </h4>
             </div>
+        <input type="text" class="form-control w-auto d-inline-block cursor-pointer" style="width:auto" name="datetimes" id="reportrange" />
+
 
         </header>
         <div class="card-body px-6 pb-6">
@@ -16,13 +18,15 @@
                         <table class="min-w-full divide-y divide-slate-100 table-fixed dark:divide-slate-700">
                             <thead class=" border-t border-slate-100 dark:border-slate-800 no-wrap">
                                 <tr>
-
+                                    <th scope="col" class="table-th">Day Serial</th>
+                                    <th scope="col" class="table-th">Title</th>
                                     <th scope="col" class="table-th">Debit Amount</th>
                                     <th scope="col" class="table-th">Credit Amount</th>
                                     <th scope="col" class="table-th">Account Balance</th>
                                     <th scope="col" class="table-th">Debit Foreign Amount</th>
                                     <th scope="col" class="table-th">Credit Foreign Amount</th>
                                     <th scope="col" class="table-th">Account Foreign Balance</th>
+                                    <th scope="col" class="table-th">Creator</th>
 
                                 </tr>
                             </thead>
@@ -30,15 +34,15 @@
 
                                 @foreach ($entries as $entry)
                                     <tr>
-                                        {{-- <td class="table-td">{{ $entry->creator->full_name }}</td> --}}
-                                        {{-- <td class="table-td">{{ $entry->day_serial }}</td> --}}
-                                        {{-- <td class="table-td">{{ $entry->entry_title->name }}</td> --}}
+                                        <td class="table-td">{{ $entry->day_serial }}</td>
+                                        <td class="table-td">{{ $entry->name }}</td>
                                         <td class="table-td">{{ number_format($entry->debit_amount, 2) }}</td>
                                         <td class="table-td">{{ number_format($entry->credit_amount, 2) }}</td>
                                         <td class="table-td">{{ number_format($entry->account_balance, 2) }}</td>
                                         <td class="table-td">{{ number_format($entry->debit_foreign_amount, 2) }}</td>
                                         <td class="table-td">{{ number_format($entry->credit_foreign_amount, 2) }}</td>
                                         <td class="table-td">{{ number_format($entry->account_foreign_balance, 2) }}</td>
+                                        <td class="table-td">{{ $entry->username }}</td>
                                     </tr>
                                 @endforeach
 
@@ -51,7 +55,7 @@
         <header class=" card-header noborder">
             <div>
                 <p class="text-xs text-slate-500 dark:text-slate-400 mt-1">End Balance</p>
-                <h4 class="card-title">{{ number_format('1800', 2) }}
+                <h4 class="card-title">{{ $entries->last() ? number_format($entries->last()->account_balance, 2) : number_format(0, 2) }}
                 </h4>
             </div>
         </header>
@@ -61,42 +65,3 @@
 
 
 </div>
-
-
-<script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
-<script>
-    $(function() {
-        var start = moment().subtract(4, 'months');
-        var end = moment();
-
-        function cb(start, end) {
-            $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-            // console.log('Start date: ' + start.format('YYYY-MM-DD'));
-            // console.log('End date: ' + end.format('YYYY-MM-DD'));
-
-            Livewire.emit('dateRangeSelected', start.format('YYYY-MM-DD'), end.format('YYYY-MM-DD'));
-        }
-
-        $('#reportrange').daterangepicker({
-            startDate: start,
-            endDate: end,
-            "alwaysShowCalendars": true,
-            ranges: {
-                'Today': [moment(), moment()],
-                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-                'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-                'This Month': [moment().startOf('month'), moment().endOf('month')],
-                // 'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1,
-                //     'month').endOf('month')],
-                'Last 3 Months': [moment().subtract(3, 'months'), moment()],
-            }
-        }, cb);
-
-        cb(start, end);
-
-
-    });
-</script>
