e0ef183ce8e039d1084d6a1525664c4b723322e1
Mina Nabil <mina9492@hotmail.com>
2024-10-03 17:27:08 +0300

    small fixes

--- resources/views/livewire/sold-policy-index.blade.php
+++ resources/views/livewire/sold-policy-index.blade.php
@@ -134,7 +134,7 @@
                                                                                 class="block date-text">{{ \Carbon\Carbon::parse($policy->expiry)->format('d-m-Y') }}</span>
                                                                         </td>
                                                                         <td class="table-td">
-                                                                            <a href="{{ route('sold.policy.show', $policy->id) }}"
+                                                                            <a href="{{ route('sold.policy.show', $policy->id) }}" target="_blank"
                                                                                 class="hover:bg-slate-900 dark:hover:bg-slate-600 dark:hover:bg-opacity-70 hover:text-white w-full border-b border-b-gray-500 border-opacity-10 px-4 py-2 text-sm dark:text-slate-300  last:mb-0 cursor-pointer first:rounded-t last:rounded-b flex space-x-2 items-center capitalize  rtl:space-x-reverse">
                                                                                 <span
                                                                                     class="block date-text">{{ $policy->policy_number }}</span>
