41950dbb965e488d4f96cf8d031c08aa54c55f7b
Michael Rafaillo <michael.rafaillo@gmail.com>
2024-10-29 16:58:03 +0300

    fix show linked commissions modal

--- resources/views/livewire/comm-profile-show.blade.php
+++ resources/views/livewire/comm-profile-show.blade.php
@@ -1549,7 +1549,7 @@
     <div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto show"
         tabindex="-1" aria-labelledby="vertically_center" aria-modal="true" role="dialog"
         style="display: block;">
-        <div class="modal-dialog top-1/2 !-translate-y-1/2 relative w-auto pointer-events-none">
+        <div class="modal-dialog relative w-auto pointer-events-none">
             <div
                 class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
                 <div class="relative bg-white rounded-lg shadow dark:bg-slate-700">
