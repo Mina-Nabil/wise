69fb76df7d8f478d42b1c4dfa5bbe10466ddc5a4
Mina Nabil <mina9492@hotmail.com>
2024-10-29 12:41:25 +0300

    debugging angel

--- app/Policies/CommProfilePolicy.php
+++ app/Policies/CommProfilePolicy.php
@@ -41,7 +41,7 @@ class CommProfilePolicy
      */
     public function create(User $user)
     {
-        return $user->is_admin || $user->is_finance;
+        return $user->is_admin;
     }
 
     /**
