77ac54597687943552bc4aa64032a7f83dfbeb8f
Mina Nabil <mina9492@hotmail.com>
2024-11-03 20:46:02 +0200

    adding followup report

--- resources/views/livewire/client-payment-index.blade.php
+++ resources/views/livewire/client-payment-index.blade.php
@@ -128,11 +128,14 @@
                             class="bg-white divide-y cursor-pointer divide-slate-100 dark:bg-slate-800 dark:divide-slate-700">
 
                             @foreach ($payments as $payment)
-                                <tr class="hover:bg-slate-200 dark:hover:bg-slate-700"
-                                    wire:click="redirectToShowPage({{ $payment->sold_policy_id }})">
+                                <tr class="hover:bg-slate-200 dark:hover:bg-slate-700">
 
                                     <td class="table-td">
-                                        {{ $payment->sold_policy->policy_number }}
+                                        <a href="{{ route('sold.policy.show', $payment->policy->id) }}" target="_blank"
+                                            class="hover:bg-slate-900 dark:hover:bg-slate-600 dark:hover:bg-opacity-70 hover:text-white w-full border-b border-b-gray-500 border-opacity-10 px-4 py-2 text-sm dark:text-slate-300  last:mb-0 cursor-pointer first:rounded-t last:rounded-b flex space-x-2 items-center capitalize  rtl:space-x-reverse">
+                                            <span
+                                                class="block date-text">{{ $payment->policy->policy_number }}</span>
+                                        </a>
                                     </td>
                                     <td class="table-td">
                                         {{ $payment->sold_policy->creator->username }}
