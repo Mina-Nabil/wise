39a9c89afda68a4a998cccc3d7154c8afc925710
Mina Nabil <mina9492@hotmail.com>
2024-11-18 12:29:50 +0200

    raglasha

--- app/Models/Payments/ClientPayment.php
+++ app/Models/Payments/ClientPayment.php
@@ -358,11 +358,14 @@ class ClientPayment extends Model
 
     public function scopeIncludeDue(Builder $query)
     {
-        return $query->join('sold_policies', 'sold_policies.id', '=', 'client_payments.sold_policy_id')
-            ->join('policy_comm_conf', function ($j) {
-                $j->on('sold_policies.policy_id', '=', 'policy_comm_conf.policy_id')
-                    ->where('is_main_penalty', 1);
-            })->select('client_payments.*', 'policy_comm_conf.due_penalty', 'policy_comm_conf.value', 'policy_comm_conf.penalty_percent', 'policy_comm_conf.calculation_type', 'sold_policies.net_premium')
+        if (!Helpers::joined($query, 'sold_policies')) {
+            $query->join('sold_policies', 'sold_policies.id', '=', 'client_payments.sold_policy_id');
+        }
+
+        $query->join('policy_comm_conf', function ($j) {
+            $j->on('sold_policies.policy_id', '=', 'policy_comm_conf.policy_id')
+                ->where('is_main_penalty', 1);
+        })->select('client_payments.*', 'policy_comm_conf.due_penalty', 'policy_comm_conf.value', 'policy_comm_conf.penalty_percent', 'policy_comm_conf.calculation_type', 'sold_policies.net_premium')
             ->selectRaw('IF( sold_policies.created_at > sold_policies.start, sold_policies.created_at , sold_policies.start)  policy_payment_due')
             ->groupBy('client_payments.id');
     }
