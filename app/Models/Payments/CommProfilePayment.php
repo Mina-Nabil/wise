062429a000a0b31ba43c0705c047d93290891de8
Mina Nabil <mina9492@hotmail.com>
2024-11-18 22:00:17 +0200

    fixing comms

--- app/Models/Payments/CommProfilePayment.php
+++ app/Models/Payments/CommProfilePayment.php
@@ -224,7 +224,7 @@ class CommProfilePayment extends Model
                 'Not set.');
             $activeSheet->getCell('C' . $i)->setValue($c->sold_policy?->policy?->company?->name . '-' . $c->sold_policy?->policy?->name);
             $activeSheet->getCell('D' . $i)->setValue($c->sold_policy->client->full_name);
-            $activeSheet->getCell('E' . $i)->setValue(number_format($c->amount));
+            $activeSheet->getCell('E' . $i)->setValue(number_format($c->pivot->amount));
             $activeSheet->getCell('F' . $i)->setValue(number_format($c->comm_percentage, 2));
             $activeSheet->getCell('G' . $i)->setValue(number_format($c->sold_policy->sales_out_comm));
             $activeSheet->getCell('H' . $i)->setValue(number_format($c->sold_policy->insured_value));
