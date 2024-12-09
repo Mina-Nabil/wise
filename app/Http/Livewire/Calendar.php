d342e192e430268b0ceedc5a594652065e11f042
Mina Nabil <mina9492@hotmail.com>
2024-12-09 15:27:28 +0200

    fixes

--- app/Http/Livewire/Calendar.php
+++ app/Http/Livewire/Calendar.php
@@ -191,7 +191,7 @@ class Calendar extends Component
     {
         $events = [];
 
-        foreach (Task::myTasksQuery(upcoming_only: true, assignedToMeOnly: true, includeWatchers: true)->get() as $t) {
+        foreach (Task::myTasksQuery(upcoming_only: true, assignedToMeOnly: true, includeWatchers: false)->get() as $t) {
             $events[] =  [
                 'id'        =>  "task" . $t->id,
                 'title'     => "T. " . $t->title,
