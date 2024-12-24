f4b9836880c5ca44c4b5ba27feaa6b65e75c5ff2
Mina Nabil <mina9492@hotmail.com>
2024-12-24 13:54:55 +0200

    sorting client payments

--- resources/views/livewire/comm-profile-show.blade.php
+++ resources/views/livewire/comm-profile-show.blade.php
@@ -810,7 +810,7 @@
                                                     </td>
 
                                                     <td class="table-td ">
-                                                        <a href="{{ route('sold.policy.show', $comm->sold_policy?->id) }}"
+                                                        <a href="{{ route('sold.policy.show', $payment->sold_policy?->id) }}"
                                                             target="_blank"
                                                             class="hover:bg-slate-900 dark:hover:bg-slate-600 dark:hover:bg-opacity-70 hover:text-white w-full border-b border-b-gray-500 border-opacity-10 px-4 py-2 text-sm dark:text-slate-300  last:mb-0 cursor-pointer first:rounded-t last:rounded-b flex space-x-2 items-center capitalize  rtl:space-x-reverse">
 
