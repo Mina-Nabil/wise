74634b2a151a1d947e3baf65dde8fe64e5842cc0
Mina Nabil <mina9492@hotmail.com>
2024-12-30 13:47:07 +0200

    fix client payment report

--- resources/views/livewire/sold-policy-show.blade.php
+++ resources/views/livewire/sold-policy-show.blade.php
@@ -487,7 +487,7 @@
                                 </span>
                                 <span class="text-lg font-medium text-slate-900 dark:text-white block">
                                     @foreach ($soldPolicy->sales_comms as $comm)
-                                    @if($soldPolicy->main_sales->id != $comm->comm_profile->id)
+                                    @if($soldPolicy->main_sales->id != $comm->comm_profile->user_id)
                                     {{$comm->comm_profile->title}} &nbsp;
                                     @endif
                                     @endforeach
