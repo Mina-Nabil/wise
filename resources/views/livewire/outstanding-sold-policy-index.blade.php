9abbe11a64118506cb79f8579a5412dd5e3d65d6
Mina Nabil <mina9492@hotmail.com>
2025-01-26 11:09:46 +0200

    fixing cancel

--- resources/views/livewire/outstanding-sold-policy-index.blade.php
+++ resources/views/livewire/outstanding-sold-policy-index.blade.php
@@ -76,9 +76,6 @@
                                                                         <th scope="col" class="table-th ">
                                                                             STATUS
                                                                         </th>
-                                                                        <th scope="col" class="table-th ">
-                                                                            ACTION
-                                                                        </th>
                                                                     </tr>
                                                                 </thead>
                                                                 <tbody class="bg-white divide-y divide-slate-100 dark:bg-slate-800 dark:divide-slate-700">
@@ -101,7 +98,13 @@
                                                                                 <span class="block date-text">{{ \Carbon\Carbon::parse($policy->expiry)->format('d-m-Y') }}</span>
                                                                             </td>
                                                                             <td class="table-td">
-                                                                                <span class="block date-text">{{ $policy->policy_number }}</span>
+                                                                                <a href="{{ route('sold.policy.show', $policy->id) }}"
+                                                                                    target="_blank"
+                                                                                    class="hover:bg-slate-900 dark:hover:bg-slate-600 dark:hover:bg-opacity-70 hover:text-white w-full border-b border-b-gray-500 border-opacity-10 px-4 py-2 text-sm dark:text-slate-300  last:mb-0 cursor-pointer first:rounded-t last:rounded-b flex space-x-2 items-center capitalize  rtl:space-x-reverse">
+                                                                                    <span class="block date-text">
+                                                                                        {{ $policy->policy_number }}
+                                                                                    </span>
+                                                                                </a>
                                                                             </td>
                                                                             <td class="table-td">
                                                                                 <div class="flex space-x-3 items-center text-left rtl:space-x-reverse">
@@ -130,34 +133,7 @@
                                                                                     <span class="badge bg-success-500 text-slate-800 text-success-500 bg-opacity-30 capitalize rounded-3xl">Validated</span>
                                                                                 @endif
                                                                             </td>
-                                                                            <td class="table-td">
-                                                                                <div class="dropstart relative">
-                                                                                    <button class="inline-flex justify-center items-center" type="button"   data-bs-toggle="dropdown" aria-expanded="false">
-                                                                                        <iconify-icon class="text-xl ltr:ml-2 rtl:mr-2" icon="heroicons-outline:dots-vertical"></iconify-icon>
-                                                                                    </button>
-                                                                                    <ul
-                                                                                        class="dropdown-menu min-w-max absolute text-sm text-slate-700 dark:text-white hidden bg-white dark:bg-slate-700 shadow z-[2] float-left overflow-hidden list-none text-left rounded-lg mt-1 m-0 bg-clip-padding border-none">
-                                                                                        <li>
-                                                                                            <a href="{{ route('sold.policy.show', $policy->id) }}"
-                                                                                                class="hover:bg-slate-900 dark:hover:bg-slate-600 dark:hover:bg-opacity-70 hover:text-white w-full border-b border-b-gray-500 border-opacity-10 px-4 py-2 text-sm dark:text-slate-300  last:mb-0 cursor-pointer first:rounded-t last:rounded-b flex space-x-2 items-center capitalize  rtl:space-x-reverse">
-                                                                                                <iconify-icon icon="heroicons-outline:eye"></iconify-icon>
-                                                                                                <span>View</span></a>
-                                                                                        </li>
-                                                                                        {{-- <li>
-                                                                                            <a href="#" data-bs-toggle="modal" data-bs-target="#editModal"
-                                                                                                class="hover:bg-slate-900 dark:hover:bg-slate-600 dark:hover:bg-opacity-70 hover:text-white w-full border-b border-b-gray-500 border-opacity-10 px-4 py-2 text-sm dark:text-slate-300 last:mb-0 cursor-pointer first:rounded-t last:rounded-b flex space-x-2 items-center capitalize rtl:space-x-reverse">
-                                                                                                <iconify-icon icon="clarity:note-edit-line"></iconify-icon>
-                                                                                                <span>Edit</span></a>
-                                                                                        </li>
-                                                                                        <li>
-                                                                                            <a href="#"
-                                                                                                class="hover:bg-slate-900 dark:hover:bg-slate-600 dark:hover:bg-opacity-70 hover:text-white w-full border-b border-b-gray-500 border-opacity-10 px-4 py-2 text-sm dark:text-slate-300 last:mb-0 cursor-pointer first:rounded-t last:rounded-b flex space-x-2 items-center capitalize rtl:space-x-reverse">
-                                                                                                <iconify-icon icon="fluent:delete-28-regular"></iconify-icon>
-                                                                                                <span>Delete</span></a>
-                                                                                        </li> --}}
-                                                                                    </ul>
-                                                                                </div>
-                                                                            </td>
+
                                                                         </tr>
                                                                     @endforeach
                                                                 </tbody>
