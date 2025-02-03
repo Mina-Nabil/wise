460ab870dd261b5da213b266aa2f4b11aad0149f
Mina Nabil <mina9492@hotmail.com>
2025-02-03 14:35:15 +0200

    raglasha payment due

---
+++ database/migrations/2025_02_03_143359_update_client_payments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('client_payments', function (Blueprint $table) {
+            $table->text('finance_note')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
