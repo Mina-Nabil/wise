c0061f8aa48bdd09bcdf6ea4b62ccd65da97e6f0
Mina Nabil <mina9492@hotmail.com>
2025-02-05 13:39:57 +0200

    finalizaing

--- app/Models/Payments/ClientPayment.php
+++ app/Models/Payments/ClientPayment.php
@@ -524,7 +524,7 @@ class ClientPayment extends Model
             $query->join('sold_policies', 'sold_policies.id', '=', 'client_payments.sold_policy_id');
         }
 
-        $query->join('policy_comm_conf', function ($j) {
+        $query->leftjoin('policy_comm_conf', function ($j) {
             $j->on('sold_policies.policy_id', '=', 'policy_comm_conf.policy_id')
                 ->where('is_main_penalty', 1);
         })->select('client_payments.*', 'policy_comm_conf.due_penalty', 'policy_comm_conf.value', 'policy_comm_conf.penalty_percent', 'policy_comm_conf.calculation_type', 'sold_policies.net_premium')
