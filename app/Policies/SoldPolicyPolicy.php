b4c42c7aa04fd8bc7ce761ef641118f8f5ba5bc7
Mina Nabil <mina9492@hotmail.com>
2025-02-06 14:06:59 +0200

    finalizaing

--- app/Policies/SoldPolicyPolicy.php
+++ app/Policies/SoldPolicyPolicy.php
@@ -89,7 +89,7 @@ class SoldPolicyPolicy
      */
     public function updatePayments(User $user, SoldPolicy $soldPolicy)
     {
-        return $user->is_admin ||  $user->is_finance || $user->id == 12 || $user->id == $soldPolicy->main_sales_id;
+        return $user->is_admin ||  $user->is_finance || $user->id == 12;
     }
 
     /**
