f625f055092a03a65b9a8f6d431cad3b129d766e
Mina Nabil <mina9492@hotmail.com>
2025-02-27 17:43:22 +0200

    fixing payment approval

--- resources/views/livewire/comm-profile-show.blade.php
+++ resources/views/livewire/comm-profile-show.blade.php
@@ -362,7 +362,8 @@
                                                                         </li>
                                                                     @endif
 
-                                                                    @if (($payment->is_new && !$payment->needs_approval) || $payment->is_approved)
+                                                                    {{-- @if (($payment->is_new && !$payment->needs_approval) || $payment->is_approved) --}}
+                                                                    @if (($payment->is_new) || $payment->is_approved)
                                                                         <li>
                                                                             <a wire:click="setPaidSec({{ $payment->id }})"
                                                                                 class="hover:bg-slate-900 dark:hover:bg-slate-600 dark:hover:bg-opacity-70 hover:text-white w-full border-b border-b-gray-500 border-opacity-10 px-4 py-2 text-sm dark:text-slate-300  last:mb-0 cursor-pointer first:rounded-t last:rounded-b flex space-x-2 items-center capitalize  rtl:space-x-reverse">
