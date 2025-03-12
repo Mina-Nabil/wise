f966da67bc1cef52c6206cd4d0880bc91c52ac17
Mina Nabil <mina9492@hotmail.com>
2025-03-12 11:24:16 +0200

    adding invoices

--- resources/views/livewire/outstanding-sold-policy-index.blade.php
+++ resources/views/livewire/outstanding-sold-policy-index.blade.php
@@ -82,6 +82,12 @@
                                     dark:hover:text-white cursor-pointer">
                                 Insurance Company</span>
                         </li>
+                        <li wire:click="togglePaymentDate">
+                            <span
+                                class="text-slate-600 dark:text-white block font-Inter font-normal px-4 py-2 hover:bg-slate-100 dark:hover:bg-slate-600
+                                dark:hover:text-white cursor-pointer">
+                                Payment Date ( From-To )</span>
+                        </li>
                     </ul>
                 </div>
             </div>
