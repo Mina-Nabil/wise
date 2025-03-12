c152a4262d0e06c115ac1b3658bc5e54a88ea4eb
Mina Nabil <mina9492@hotmail.com>
2025-03-12 12:53:16 +0200

    adding sold policy filters

--- resources/views/livewire/outstanding-sold-policy-index.blade.php
+++ resources/views/livewire/outstanding-sold-policy-index.blade.php
@@ -99,7 +99,7 @@
                                 <div class="flex flex-col space-y-2">
                                     <span class="text-slate-600 dark:text-white font-Inter font-normal"
                                         wire:click="toggleHasInvoice(true)">
-                                        Has Invoice
+                                        Has Invoice?
                                     </span>
                                 </div>
                             </div>
@@ -108,7 +108,7 @@
                             <div class="px-4 py-2">
                                 <div class="flex flex-col space-y-2">
                                     <span class="text-slate-600 dark:text-white font-Inter font-normal"  wire:click="toggleInvoicePaid">
-                                        Invoice Payment Status
+                                        Invoice Paid?
                                     </span>
                                 </div>
                             </div>
