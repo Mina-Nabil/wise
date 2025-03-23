dc64bc882f35467d84294b33e61e87021a8e3e6b
Mina Nabil <mina9492@hotmail.com>
2025-03-23 10:34:44 +0200

    fixing finance assistant

--- app/Http/Livewire/SoldPolicyIndex.php
+++ app/Http/Livewire/SoldPolicyIndex.php
@@ -243,7 +243,7 @@ class SoldPolicyIndex extends Component
                 $endDate = Carbon::parse($this->endDate);
                 return $query->fromTo($startDate, $endDate);
             })
-            ->curosPaginate(20);
+            ->cursorPaginate(20);
             
         $PAYMENT_FREQS = OfferOption::PAYMENT_FREQS;
 
