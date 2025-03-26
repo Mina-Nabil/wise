00acc0776dd005ed806fda08a7a3554b82685014
Mina Nabil <mina9492@hotmail.com>
2025-03-26 10:20:25 +0200

    fixing accounting

--- routes/web.php
+++ routes/web.php
@@ -125,10 +125,10 @@ Route::middleware('auth', 'active')->group(function () {
         return response()->redirectTo('/accounts/main');
     });
     Route::get('/accounts/entries', JournalEntryIndex::class);
-    Route::get('/accounts/entries/{id}', JournalEntryIndex::class)->name('accounts.entries');
-    Route::get('/accounts/titles', EntryTitleIndex::class);
     Route::get('/accounts/entries/new', CreateJournalEntry::class);
     Route::get('/accounts/entries/unapproved', UnapprovedEntryIndex::class);
+    Route::get('/accounts/entries/{id}', JournalEntryIndex::class)->name('accounts.entries');
+    Route::get('/accounts/titles', EntryTitleIndex::class);
     Route::get('/accounts/gettree/{id}', function ($id) {
         return response()->json(Account::findOrFail($id)->getTree());
     });
