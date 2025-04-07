bbfe5f2f1b19f4d817638adc5a10ff72da70ee48
Mina Nabil <mina9492@hotmail.com>
2025-04-07 14:07:54 +0200

    report fix

--- resources/views/livewire/sold-policy-index.blade.php
+++ resources/views/livewire/sold-policy-index.blade.php
@@ -134,7 +134,7 @@
                                                                                                 class="text-base cursor-help"></iconify-icon>
                                                                                                 <script>
                                                                                                     tippy('#comment-icon-{{ $policy->id }}', {
-                                                                                                        content: '{{ $policy->review_comment }}',
+                                                                                                        content: `{{ htmlentities($policy->review_comment) }} `,
                                                                                                     });
                                                                                                 </script>
                                                                                         @endif
