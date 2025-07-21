fcbc20c60b464e2d56cfed10785707f26025793e
Mina Nabil <mina9492@hotmail.com>
2025-07-21 13:36:03 +0300

    small fixes

--- resources/views/livewire/sold-policy-show.blade.php
+++ resources/views/livewire/sold-policy-show.blade.php
@@ -1718,7 +1718,7 @@
                                                             <ul
                                                                 class="dropdown-menu min-w-max absolute text-sm text-slate-700 dark:text-white hidden bg-white dark:bg-slate-700 shadow z-[2] float-left overflow-hidden list-none text-left rounded-lg mt-1 m-0 bg-clip-padding border-none">
                                                                 @if ($payment->is_new || is_Null($payment->status))
-                                                                    @can('update', $payment)
+                                                                    @can('updateClientPayments', $soldPolicy)
                                                                         <li>
                                                                             <a wire:click="openEditPaymentSec({{ $payment->id }})"
                                                                                 class="hover:bg-slate-900 dark:hover:bg-slate-600 dark:hover:bg-opacity-70 hover:text-white w-full border-b border-b-gray-500 border-opacity-10 px-4 py-2 text-sm dark:text-slate-300  last:mb-0 cursor-pointer first:rounded-t last:rounded-b flex space-x-2 items-center capitalize  rtl:space-x-reverse">
@@ -1779,7 +1779,7 @@
                                                                 @endif
 
                                                                 @if ($payment->doc_url)
-                                                                    @can('update', $payment)
+                                                                @can('updateClientPayments', $soldPolicy)
                                                                         <li>
                                                                             <a wire:click="ConfirmRemovePaymentDoc({{ $payment->id }})"
                                                                                 class="hover:bg-slate-900 dark:hover:bg-slate-600 dark:hover:bg-opacity-70 hover:text-white w-full border-b border-b-gray-500 border-opacity-10 px-4 py-2 text-sm dark:text-slate-300  last:mb-0 cursor-pointer first:rounded-t last:rounded-b flex space-x-2 items-center capitalize  rtl:space-x-reverse">
@@ -1790,7 +1790,7 @@
                                                                         </li>
                                                                     @endcan
                                                                 @else
-                                                                    @can('update', $payment)
+                                                                @can('updateClientPayments', $soldPolicy)
                                                                         <li>
                                                                             <label for="paymentDoc"
                                                                                 wire:click="setPaymentDoc({{ $payment->id }})"
