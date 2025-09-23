bc390628fbb51a37e835856cbd4ff6289d9cdfa9
Mina-SS <mina.nabil@safetyshieldglobal.com>
2025-09-23 10:47:26 +0300

    linting

--- app/Models/Tasks/Task.php
+++ app/Models/Tasks/Task.php
@@ -15,6 +15,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Auth;
