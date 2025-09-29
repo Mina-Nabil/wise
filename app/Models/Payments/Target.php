35a7ce591372076ba913d368f926b144887f8d20
Mina-SS <mina.nabil@safetyshieldglobal.com>
2025-09-29 16:59:51 +0300

    linting

--- app/Models/Payments/Target.php
+++ app/Models/Payments/Target.php
@@ -65,15 +65,13 @@ class Target extends Model
             $sp->calculateTotalSalesOutComm();
             $tmpAmount = (($sp->tax_amount > 0) ? $sp->after_tax_comm : $sp->after_tax_comm * .95) - $sp->total_comm_subtractions;
             $totalIncome += $tmpAmount;
-            if ($sp->id == 4387) {
-                Log::info("SP#$sp->id details", ["tmpAmount" => $tmpAmount, "tax_amount" => $sp->tax_amount, "after_tax_comm" => $sp->after_tax_comm, "client_paid_by_dates" => $sp->client_paid_by_dates, "gross_premium" => $sp->gross_premium, "total_comm_subtractions" => $sp->total_comm_subtractions]);
-            }
+
+            Log::info("SP#$sp->id details", ["tmpAmount" => $tmpAmount, "tax_amount" => $sp->tax_amount, "after_tax_comm" => $sp->after_tax_comm, "client_paid_by_dates" => $sp->client_paid_by_dates, "gross_premium" => $sp->gross_premium, "total_comm_subtractions" => $sp->total_comm_subtractions]);
         }
         foreach ($soldPolicies as $sp) {
             $paidAmountsPercent[$sp->id] = (($sp->tax_amount ? $sp->after_tax_comm : $sp->after_tax_comm * .95) - $sp->total_comm_subtractions) / $totalIncome;
-            if ($sp->id == 4387) {
-                Log::info("SP#$sp->id paidAmountsPercent", ["paidAmountsPercent" => $paidAmountsPercent[$sp->id]]);
-            }
+
+            Log::info("SP#$sp->id paidAmountsPercent", ["paidAmountsPercent" => $paidAmountsPercent[$sp->id]]);
         }
 
 
