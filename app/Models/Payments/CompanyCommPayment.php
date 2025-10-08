2ce947bc95ede7f5ba3e4536f9d02501e77ba9c5
Mina-SS <mina.nabil@safetyshieldglobal.com>
2025-10-08 16:41:23 +0300

    fix

--- app/Models/Payments/CompanyCommPayment.php
+++ app/Models/Payments/CompanyCommPayment.php
@@ -275,8 +275,8 @@ class CompanyCommPayment extends Model
             $activeSheet->getCell('C' . $i)->setValue($payment->sold_policy && $payment->sold_policy->created_at ? Carbon::parse($payment->sold_policy->created_at)->format('d/m/Y') : '');
             $activeSheet->getCell('D' . $i)->setValue($payment->payment_date ? Carbon::parse($payment->payment_date)->format('d/m/Y') : 'N/A');
             $activeSheet->getCell('E' . $i)->setValue(number_format($payment->sold_policy->net_premium, 2, '.', ','));
-            $activeSheet->getCell('F' . $i)->setValue(number_format($payment->amount + $payment->tax_amount, 2, '.', ','));
-            $activeSheet->getCell('G' . $i)->setValue(number_format(0, 2, '.', ','));
+            $activeSheet->getCell('F' . $i)->setValue(number_format($payment->amount, 2, '.', ','));
+            $activeSheet->getCell('G' . $i)->setValue(number_format($payment->tax_amount, 2, '.', ','));
             // $activeSheet->getCell('F' . $i)->setValue(number_format($payment->amount - $payment->tax_amount, 2, '.', ','));
             // $activeSheet->getCell('G' . $i)->setValue(number_format($payment->tax_amount, 2, '.', ','));
             $activeSheet->getCell('H' . $i)->setValue(ucfirst($payment->status));
