e230cab924e3f3ef90c3cd99f4b02a92d9a33fdf
Mina-SS <mina.nabil@safetyshieldglobal.com>
2025-11-02 14:49:18 +0200

    edafat

--- app/Http/Livewire/ClientPaymentIndex.php
+++ app/Http/Livewire/ClientPaymentIndex.php
@@ -19,7 +19,7 @@ class ClientPaymentIndex extends Component
     public $endDate;
     public $types = [];
     public $section = 'all';
-    
+
 
     protected $queryString = [
         'section',
@@ -29,7 +29,7 @@ class ClientPaymentIndex extends Component
 
     public function getTypeIcon($type)
     {
-        return match($type) {
+        return match ($type) {
             ClientPayment::PYMT_TYPE_CASH => 'heroicons-outline:cash',
             ClientPayment::PYMT_TYPE_CHEQUE => 'heroicons-outline:document-text',
             ClientPayment::PYMT_TYPE_BANK_TRNSFR => 'heroicons-outline:credit-card',
@@ -84,22 +84,23 @@ class ClientPaymentIndex extends Component
     {
         $statuses = ClientPayment::PYMT_STATES;
         $PYMT_TYPES = ClientPayment::PYMT_TYPES;
-        
+
         $paymentsQuery = ClientPayment::userData($this->filteredStatus, $this->myPayments, $this->searchText)
             ->when(count($this->types), fn($q) => $q->byTypes($this->types))
             ->when($this->startDate && $this->endDate, function ($query) {
                 $startDate = $this->startDate ? Carbon::parse($this->startDate) : null;
                 $endDate = $this->endDate ? Carbon::parse($this->endDate) : null;
                 return $query->byDateRange($startDate, $endDate);
-            })
-            ->with('sold_policy', 'sold_policy.client', 'sold_policy.creator', 'assigned');
+            });
 
         $totalPayments = $paymentsQuery
-        ->clone()
-        ->select('client_payments.id', 'client_payments.amount')
-        ->groupBy('client_payments.id')
-        ->selectRaw('SUM(client_payments.amount) as total_amount')->first()->total_amount;
-        $payments = $paymentsQuery->paginate(50);
+            ->clone()
+            ->select('client_payments.id', 'client_payments.amount')
+            ->groupBy('client_payments.id')
+            ->selectRaw('SUM(client_payments.amount) as total_amount')->first()->total_amount;
+        $payments = $paymentsQuery
+            ->with('sold_policy', 'sold_policy.client', 'sold_policy.creator', 'assigned')
+            ->paginate(50);
 
         return view('livewire.client-payment-index', [
             'statuses' => $statuses,
