466490cd1abed807ae66bfbf538e7e3bb696ae10
Mina-Nabil <minanabil284@gmail.com>
2025-11-15 14:38:09 +0200

    add dates

--- resources/views/livewire/offer-index.blade.php
+++ resources/views/livewire/offer-index.blade.php
@@ -114,7 +114,7 @@
         <div class="card-body px-6 pb-6">
             <div class=" -mx-6">
                 <div class="inline-block min-w-full align-middle">
-                    <div >
+                    <div class="overflow-x-auto">
                         <table class="min-w-full divide-y divide-slate-100 table-fixed dark:divide-slate-700">
                             <thead
                                 class=" border-t border-slate-100 dark:border-slate-800 bg-slate-200 dark:bg-slate-700">
