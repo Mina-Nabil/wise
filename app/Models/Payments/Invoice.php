7a956ca26a8af921508b66b745fdaf3dde9a260e
Mina-Nabil <minanabil284@gmail.com>
2025-11-23 09:53:50 +0200

    fixes

--- app/Models/Payments/Invoice.php
+++ app/Models/Payments/Invoice.php
@@ -11,6 +11,7 @@ use App\Models\Users\AppLog;
 use App\Models\Users\User;
 use Carbon\Carbon;
 use Exception;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
