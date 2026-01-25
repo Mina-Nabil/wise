14d373ea4e9e63e3e8a4bb644713bd08c28243d4
Mina-Nabil <minanabil284@gmail.com>
2026-01-25 13:02:27 +0200

    fixes

--- app/Models/Payments/Invoice.php
+++ app/Models/Payments/Invoice.php
@@ -413,7 +413,7 @@ class Invoice extends Model
         if (!$journalEntry) {
             return 0;
         }
-        $account = $journalEntry->accounts->wherePivot('account_id', Account::TRANS_FEES_ACCOUNT_ID)->first();
+        $account = $journalEntry->accounts->where('id', Account::TRANS_FEES_ACCOUNT_ID)->first();
         return $account->pivot->amount ?? 5;
     }
 }
