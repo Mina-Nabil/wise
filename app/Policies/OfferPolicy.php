b7fab3ffc9f7c8e771bfc9cce32cf7c252488c63
Mina-Nabil <minanabil284@gmail.com>
2026-04-06 15:10:44 +0200

    fixes

--- app/Policies/OfferPolicy.php
+++ app/Policies/OfferPolicy.php
@@ -201,7 +201,7 @@ class OfferPolicy
 
     public function lock(User $user, Offer $offer)
     {
-        return ($user->is_admin || $offer->is_operations) && $offer->assignee_id == $user->id;
+        return $user->is_admin || ($offer->is_operations && ($offer->assignee_id == $user->id || $offer->assignee_type == $user->type));
     }
 
     public function unlock(User $user, Offer $offer)
